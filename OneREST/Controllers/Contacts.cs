524039a3fb026dc66aced980c64c9ad2bd4a8f82
Carl Domingue <carl.domingue@gcpsk12.org>
2023-04-11 13:02:45 -0400

    first commit

---
+++ OneREST/Controllers/Contacts.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using OneREST.Models;
+namespace OneREST.Controllers;
+
+[ApiController]
+[Route("/api/contacts")]
+public class Contacts : ControllerBase
+{
+    string connectionString = "Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=admintc";
+
+    public Contacts()
+    {
+    }
+
+    [HttpGet]
+    public List<PhoneContact> GetAll()
+    {
+        List<PhoneContact> contacts = new List<PhoneContact>();
+
+        using (var conn = new NpgsqlConnection(connectionString))
+        {
+            conn.Open();
+            // Retrieve data from the "contacts" table
+            using (var cmd = new NpgsqlCommand("SELECT name, phone FROM contactsdotnet", conn))
+            {
+                var reader = cmd.ExecuteReader();
+                {
+                    while (reader.Read())
+                    {
+                        // Retrieve data from the current row
+                        string name = reader.GetString(0);
+                        string phone = reader.GetString(1);
+
+                        // Create a Contact object and display the data
+                        PhoneContact contact = new PhoneContact(name, phone);
+                        contacts.Add(contact);
+                        Console.WriteLine("Name: {contact.name}, Phone: {contact.phone}");
+                    }
+                }
+            }
+        }
+        return contacts;
+    }
+
+
+
+    [HttpGet("{id}")]
+    public PhoneContact GetID(int id)
+    {
+        return null;
+    }
+
+    [HttpPost("{id}")]
+    public PhoneContact Insert(int id)
+    {
+        return null;
+    }
+
+}
+
---
+++ OneREST/Models/PhoneContact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace OneREST.Models;
+
+public class PhoneContact
+{
+        public String name { get; set; }
+        public String phone { get; set; }
+
+        public PhoneContact(String name, String phone)
+	{
+		this.name = name;
+		this.phone = phone;
+	}
+
+}
+
